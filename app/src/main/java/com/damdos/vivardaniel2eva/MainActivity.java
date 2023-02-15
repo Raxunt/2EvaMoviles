a93024c5c690a819e8e368293cc70e3322baada5
DaniV <ryr01932@educastur.es>
2023-02-15 18:54:21 +0100

    Segundo commit 2

--- app/src/main/java/com/damdos/vivardaniel2eva/MainActivity.java
+++ app/src/main/java/com/damdos/vivardaniel2eva/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
                     editor.putBoolean("night", true);
 
                 }
+
                 editor.apply();
             }
         });
